6387633c971122f3194c87647114c43fda152cdf
Allan Roscoche <allanroscoche@gmail.com>
2020-09-01 10:47:27 -0300

    Refactor confirmButton

--- components/buttons/confirm.js
+++ components/buttons/confirm.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function ConfirmButton(props) {
     return (
-        <Link href={props.url} >
-            <Button variant="contained" color="primary">
+        <Link href={props.url} passHref>
+            <Button variant="contained" color="primary" as="a">
                 {props.children}
             </Button>
         </Link>
-        );
+    );
 }
