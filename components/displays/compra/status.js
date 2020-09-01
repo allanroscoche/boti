10f6d85d8c8deb04fdc7fda0eb0c97c0f47a02b2
Allan Roscoche <allanroscoche@gmail.com>
2020-09-01 10:31:22 -0300

    Refactor index page

---
+++ components/displays/compra/status.js
@@ -0,0 +1,5 @@
+import { Typography } from "@material-ui/core";
+
+export default function StatusDisplay({status}) {
+    return (<Typography>Status: {status}</Typography>);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
+import { Grid, Button } from "@material-ui/core";
 import Link from "next/link";
 
 export default function Home()
@@ -7,16 +6,26 @@ export default function Home()
 	const routes = [
 		{ url:"/login", title: "Login" },
 		{ url:"/compras", title: "Compras" },
+		{ url:"/compras/nova", title: "Nova Compra" },
 		{ url:"/cashback", title: "Cashback" },
 		{ url:"/revendedor", title: "Revendedor" }
 	];
 	return (
-		<Menu id="route-menu" open={true} >
+		<Grid container 
+		alignItems="center"
+		justify="center"
+		direction="column"
+		spacing={2}
+		id="route-menu"  >
 			{
-				routes.map( ({url, title}) => (
-					<MenuItem key={url}><Link href={url}>{title}</Link></MenuItem>
+				routes.map( ({url, title}, index) => (
+					<Grid item key={index} xs={8}>
+						<Link href={url} >
+							<Button variant="contained">{title}</Button>
+						</Link>
+					</Grid>
 				))
 			}
-		</Menu>
+		</Grid>
 	);
 }
